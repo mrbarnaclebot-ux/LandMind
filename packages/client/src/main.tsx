67f7a0df6f71cba0fdbfddd30edbb88698776a48
mrbarnaclebot-ux <mrbarnaclebot@gmail.com>
2026-01-20 17:57:21 +0530

    feat(04-01): wrap App with SolanaProvider in main.tsx
    - Import SolanaProvider from providers
    - Wrap App component with wallet context
    - Enables wallet hooks throughout application

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { SolanaProvider } from './providers/SolanaProvider';
 import App from './App';
 import './App.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SolanaProvider>
+      <App />
+    </SolanaProvider>
   </StrictMode>
 );
